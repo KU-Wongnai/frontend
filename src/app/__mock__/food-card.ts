9c18c2f6f8579604652c9aabcde8b77227abf7cf
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-09-20 22:14:24 +0700

    ui(show_menu): add modal to show the menu

--- src/app/__mock__/food-card.ts
+++ src/app/__mock__/food-card.ts
@@ -2,6 +2,7 @@ export const mockFoodData = Array.from({ length: 30 }, (_, index) => ({
   id: (index + 1).toString(),
   imageUrl:
     "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+  description : `Food Description ${index + 1}`,
   name: `Food Name ${index + 1}`,
   category: `Type ${index + 1}`,
   price: 30 + index * 10,
--- src/app/interfaces/foodCard.ts
+++ src/app/interfaces/foodCard.ts
@@ -2,6 +2,7 @@ interface FoodCardProps {
   id: string;
   imageUrl: string | null;
   name: string;
+  description: string;
   category: string;
   price: number;
   href: string;
