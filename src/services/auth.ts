7d94e097b5dcfa5f4380ee79df4585403a462ff5
Qu1etboy <nonzagreanthai@gmail.com>
2023-09-25 02:36:41 +0700

    fix: add types and api url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,12 @@
 import { API_URL, httpClient } from "@/lib/http-client";
+import { LoginForm, RegisterForm } from "@/validations/auth-schema";
 
-export const signUp = async (data: any) => {
+export const signUp = async (data: RegisterForm) => {
   const { data: user } = await httpClient.post("user/register", data);
   return user;
 };
 
-export const login = async (data: any) => {
+export const login = async (data: LoginForm) => {
   const { data: token } = await httpClient.post("user/api/auth/login", data);
   localStorage.setItem("token", token.access_token);
 };
