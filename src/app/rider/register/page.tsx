eb928db906c6e56afedee73e580d46079d096c09
Sittipong Hemloun <ong22280@gmail.com>
2023-10-14 19:17:25 +0700

    I forgot responsive some page

--- src/app/rider/register/page.tsx
+++ src/app/rider/register/page.tsx
@@ -6,40 +6,37 @@ import Link from "next/link";
 
 const SelectRegister = () => {
   return (
-    <div className="flex flex-row gap-x-28 items-center justify-center min-h-screen bg-background">
-      <Link
-        href="/rider/register/general"
-        className="w-96 h-96 bg-card rounded-md border shadow-md hover:scale-105 transition-all duration-300"
-      >
-        <h1 className="text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-primary p-4">
-          Register as <span className="text-secondary-foreground">General</span>
-        </h1>
-        <div className="flex align-middle h-72">
-          <Image
-            src={GeneralRider}
-            width={300}
-            height={300}
-            alt="General Rider"
-            className="mx-auto my-auto"
-          />
+    <div className="flex flex-col md:flex-row gap-6 md:gap-28 items-center justify-center min-h-screen bg-background">
+      <Link href="/rider/register/general">
+        <div className="flex flex-col w-72 md:w-96 h-72 md:h-96 bg-card rounded-md border shadow-md hover:scale-105 transition-all duration-300 p-4 items-center justify-between text-center">
+          <h1 className="text-3xl font-semibold tracking-tight text-primary mb-4">
+            Register as{" "}
+            <span className="text-secondary-foreground">General</span>
+          </h1>
+          <div className=" md:h-72 flex">
+            <Image
+              src={GeneralRider}
+              width={300}
+              height={300}
+              alt="General Rider"
+            />
+          </div>
         </div>
       </Link>
-      <Link
-        href="/rider/register/student"
-        // smooth scale on hover
-        className="w-96 h-96 bg-card rounded-md border shadow-md hover:scale-105 transition-all duration-300"
-      >
-        <h1 className="text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-primary p-4">
-          Register as <span className="text-secondary-foreground">Student</span>
-        </h1>
-        <div className="flex align-middle h-72">
-          <Image
-            src={StudentRider}
-            width={300}
-            height={300}
-            alt="General Rider"
-            className="mx-auto left-4 top-4 md:left-8 md:top-8 z-10"
-          />
+      <Link href="/rider/register/student">
+        <div className="flex flex-col w-72 md:w-96 h-72 md:h-96 bg-card rounded-md border shadow-md hover:scale-105 transition-all duration-300 p-4 items-center justify-between text-center">
+          <h1 className="text-3xl font-semibold tracking-tight text-primary mb-4">
+            Register as{" "}
+            <span className="text-secondary-foreground">Student</span>
+          </h1>
+          <div className=" md:h-72 flex">
+            <Image
+              src={StudentRider}
+              width={300}
+              height={300}
+              alt="Student Rider"
+            />
+          </div>
         </div>
       </Link>
     </div>
