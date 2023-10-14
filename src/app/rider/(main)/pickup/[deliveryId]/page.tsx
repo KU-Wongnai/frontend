bd3d4d7aa1f32d70787440cdc24f10e1db3b691f
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-14 10:13:28 +0700

    feat(delivery): add loading skeleton

--- src/app/rider/(main)/pickup/[deliveryId]/page.tsx
+++ src/app/rider/(main)/pickup/[deliveryId]/page.tsx
@@ -21,11 +21,13 @@ import React, { useEffect } from "react";
 
 const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
   const [deliveryOrder, setDeliveryOrder] = React.useState<Delivery>();
+  const [loading, setLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getDelivery(params.deliveryId);
       setDeliveryOrder(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -35,9 +37,20 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
       <Card>
         <CardHeader>
           <CardTitle>
-            Order{" "}
-            <span className="text-primary">#{deliveryOrder?.order.id}</span>{" "}
-            Details
+            <div className="flex items-center gap-2">
+              Order
+              <div className="flex items-center text-primary">
+                #
+                {loading ? (
+                  <div className="animate-pulse ml-2 h-2.5 bg-green-200 rounded-full dark:bg-green-700 w-48">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <span>{deliveryOrder?.order.id}</span>
+                )}
+              </div>
+              Details
+            </div>
           </CardTitle>
         </CardHeader>
         <CardContent>
@@ -59,7 +72,13 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                       {deliveryOrder?.order.user?.name?.charAt(0)}
                     </AvatarFallback>
                   </Avatar>
-                  {deliveryOrder?.order?.user?.name}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-48">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.order?.user?.name}</span>
+                  )}
                 </div>
               </div>
               <div className="flex items-center justify-between">
@@ -67,7 +86,13 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                   <Mail /> Email
                 </div>
                 <div className="text-sm sm:text-base font-medium">
-                  {deliveryOrder?.order?.user?.email}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-32">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.order?.user?.email}</span>
+                  )}
                 </div>
               </div>
               <div className="flex items-center justify-between">
@@ -75,7 +100,15 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                   <Phone /> Contact
                 </div>
                 <div className="text-sm sm:text-base font-medium">
-                  {formatPhoneNumber(deliveryOrder?.contactInfo || "")}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-24">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>
+                      {formatPhoneNumber(deliveryOrder?.contactInfo || "")}
+                    </span>
+                  )}
                 </div>
               </div>
             </div>
@@ -97,7 +130,13 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                       {deliveryOrder?.order.restaurant?.name?.charAt(0)}
                     </AvatarFallback>
                   </Avatar>
-                  {deliveryOrder?.order.restaurant?.name}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-48">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.order?.restaurant?.name}</span>
+                  )}
                 </div>
               </div>
               <div className="flex justify-between">
@@ -106,7 +145,13 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                   Location
                 </div>
                 <div className="text-sm sm:text-base font-medium">
-                  {deliveryOrder?.order.restaurant?.location}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-52">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.order?.restaurant?.location}</span>
+                  )}
                 </div>
               </div>
               <div className="flex justify-between">
@@ -115,16 +160,28 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
                   Contact
                 </div>
                 <div className="text-sm sm:text-base font-medium">
-                  {deliveryOrder?.order.restaurant?.contactInfo}
+                  {loading ? (
+                    <div className="animate-pulse h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-24">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.order?.restaurant?.contactInfo}</span>
+                  )}
                 </div>
               </div>
               <div className="flex justify-between">
-                <div className="flex items-center gap-1 text-sm sm:text-base font-medium">
+                <div className="animate-pulse flex items-center gap-1 text-sm sm:text-base font-medium">
                   <MapPin />
                   Delivery Location
                 </div>
                 <div className="text-sm sm:text-base font-medium">
-                  {deliveryOrder?.deliveryAddress}
+                  {loading ? (
+                    <div className="h-2.5 bg-neutral-200 rounded-full dark:bg-neutral-700 w-52">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  ) : (
+                    <span>{deliveryOrder?.deliveryAddress}</span>
+                  )}
                 </div>
               </div>
             </div>
@@ -133,32 +190,56 @@ const DeliveryDetails = ({ params }: { params: { deliveryId: string } }) => {
               <h3 className="text-sm sm:text-base font-medium mb-1 sm:mb-2 text-left">
                 Order Items
               </h3>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="bg-muted">#</TableHead>
-                    <TableHead className="bg-muted">Menu Item</TableHead>
-                    <TableHead className="bg-muted">Quantity</TableHead>
-                    <TableHead className="bg-muted">Amount</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {deliveryOrder?.order &&
-                    deliveryOrder?.order.orderItems.map((item) => (
-                      <TableRow key={item.id}>
-                        <TableCell>{item.menu.id}</TableCell>
-                        <TableCell>{item.menu.name}</TableCell>
-                        <TableCell>x{item.quantity}</TableCell>
-                        <TableCell>
-                          {new Intl.NumberFormat("en-US", {
-                            style: "currency",
-                            currency: "THB",
-                          }).format(item.price)}
-                        </TableCell>
+              <>
+                {loading ? (
+                  <div role="status" className="w-full flex justify-center">
+                    <svg
+                      aria-hidden="true"
+                      className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="bg-muted">#</TableHead>
+                        <TableHead className="bg-muted">Menu Item</TableHead>
+                        <TableHead className="bg-muted">Quantity</TableHead>
+                        <TableHead className="bg-muted">Amount</TableHead>
                       </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
+                    </TableHeader>
+                    <TableBody>
+                      {deliveryOrder?.order &&
+                        deliveryOrder?.order.orderItems.map((item) => (
+                          <TableRow key={item.id}>
+                            <TableCell>{item.menu.id}</TableCell>
+                            <TableCell>{item.menu.name}</TableCell>
+                            <TableCell>x{item.quantity}</TableCell>
+                            <TableCell>
+                              {new Intl.NumberFormat("en-US", {
+                                style: "currency",
+                                currency: "THB",
+                              }).format(item.price)}
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                  </Table>
+                )}
+              </>
             </div>
             <div>
               <h3 className="text-sm sm:text-base font-medium mb-2 text-left">
--- src/app/rider/(main)/pickup/page.tsx
+++ src/app/rider/(main)/pickup/page.tsx
@@ -18,12 +18,14 @@ import toast from "react-hot-toast";
 
 const OrderPickupPage = () => {
   const [deliveryOrders, setDeliveryOrders] = useState<Delivery[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getUnassignedDeliveryOrders();
 
       setDeliveryOrders(data);
+      setLoading(false);
     };
 
     fetchData();
@@ -50,49 +52,66 @@ const OrderPickupPage = () => {
         Choose an order to start delivering
       </p>
       <section className="my-6">
-        <div className="space-y-4">
-          {deliveryOrders.map((d) => (
-            <Card>
-              <CardHeader>
-                <div className="flex gap-3">
-                  <Avatar>
-                    <AvatarImage
-                      src={d.order.restaurant?.image || ""}
-                      alt={d.order.restaurant?.name}
-                    ></AvatarImage>
-                    <AvatarFallback>
-                      {d.order.restaurant?.name?.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <CardTitle>{d.order.restaurant?.name}</CardTitle>
-                    <CardDescription>Order #{d.order.id}</CardDescription>
+        {loading ? (
+          <div className="space-y-4">
+            {new Array(5).fill(0).map((_, i) => (
+              <div
+                role="status"
+                className="flex items-center justify-center h-72 w-full bg-neutral-300 rounded-lg animate-pulse dark:bg-neutral-700"
+              >
+                <span className="sr-only">Loading...</span>
+              </div>
+            ))}
+          </div>
+        ) : deliveryOrders.length > 0 ? (
+          <div className="space-y-4">
+            {deliveryOrders.map((d) => (
+              <Card>
+                <CardHeader>
+                  <div className="flex gap-3">
+                    <Avatar>
+                      <AvatarImage
+                        src={d.order.restaurant?.image || ""}
+                        alt={d.order.restaurant?.name}
+                      ></AvatarImage>
+                      <AvatarFallback>
+                        {d.order.restaurant?.name?.charAt(0)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <CardTitle>{d.order.restaurant?.name}</CardTitle>
+                      <CardDescription>Order #{d.order.id}</CardDescription>
+                    </div>
                   </div>
-                </div>
-              </CardHeader>
-              <CardContent className="space-y-4">
-                <div>
-                  <p className="text-gray-600 dark:text-gray-300 text-sm">
-                    Address
-                  </p>
-                  <span>{d.deliveryAddress}</span>
-                </div>
-                <div>
-                  <p className="text-gray-600 dark:text-gray-300 text-sm">
-                    Items
-                  </p>
-                  <span>{d.order.orderItems.length}</span>
-                </div>
-              </CardContent>
-              <CardFooter className="space-x-3">
-                <Button onClick={() => handleAccept(d.id)}>Accept</Button>
-                <Button variant="outline" asChild>
-                  <Link href={`/rider/pickup/${d.id}`}>View Details</Link>
-                </Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <div>
+                    <p className="text-gray-600 dark:text-gray-300 text-sm">
+                      Address
+                    </p>
+                    <span>{d.deliveryAddress}</span>
+                  </div>
+                  <div>
+                    <p className="text-gray-600 dark:text-gray-300 text-sm">
+                      Items
+                    </p>
+                    <span>{d.order.orderItems.length}</span>
+                  </div>
+                </CardContent>
+                <CardFooter className="space-x-3">
+                  <Button onClick={() => handleAccept(d.id)}>Accept</Button>
+                  <Button variant="outline" asChild>
+                    <Link href={`/rider/pickup/${d.id}`}>View Details</Link>
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center mt-12">
+            No order delivery at this moment.
+          </div>
+        )}
       </section>
     </main>
   );
