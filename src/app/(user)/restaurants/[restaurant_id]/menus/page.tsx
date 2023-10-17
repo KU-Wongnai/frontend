fe915f307b0dbb8b9aa7b70128fbd2e9cdba62df
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-17 20:42:53 +0700

    feat(ui): select menu and menu options

--- src/app/(user)/restaurants/[restaurant_id]/menus/page.tsx
+++ src/app/(user)/restaurants/[restaurant_id]/menus/page.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import RestaurantCardDetail from "@/app/(user)/restaurants/[restaurant_id]/components/restaurant-card-detail";
 import MenuCardList from "../components/menu-card-list";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 type Props = {
   params: {
@@ -21,16 +22,16 @@ const RestaurantMenuPage: React.FC<Props> = ({
     <main className="container mx-auto py-6 px-2 sm:px-4 md:px-6 lg:px-8">
       {/* food card Horizontal */}
       <RestaurantCardDetail id={params.restaurant_id} />
-
       {/* all menu */}
-      <section className="w-full bg-card pt-3 border shadow-sm rounded-lg p-5 mt-6">
-        <div className="flex border-b mb-3 gap-2">
-          {/* <ChefHat className="text-green-600 w-8 h-8" /> */}
-          <h1 className="border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-primary">
-            All menu
-          </h1>
-        </div>
-        <MenuCardList id={params.restaurant_id} />
+      <section className="w-full mt-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>All Menu</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <MenuCardList id={params.restaurant_id} />
+          </CardContent>
+        </Card>
       </section>
     </main>
   );
