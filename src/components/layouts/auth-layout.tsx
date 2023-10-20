2512381cbe459d0e2feff83fabc01aa61acc3da6
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-20 13:56:11 +0700

    fix(protected-route): cannot access auth route when logged in

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import useAuthStore from "@/contexts/auth-store";
-import { redirect } from "next/navigation";
-import React from "react";
+import useStore from "@/contexts/useStore";
+import { redirect, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   const user = useAuthStore((state) => state.user);
+  const [loading, setLoading] = useState(true);
 
-  if (!user) {
-    redirect("/auth");
+  useEffect(() => {
+    if (!user) {
+      redirect("/auth");
+    }
+    setLoading(false);
+  }, [user]);
+
+  if (loading) {
+    return null;
   }
 
   return <>{children}</>;
