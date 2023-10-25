db7e8d8d62a957227d32a1d2e635f5718c51e143
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-25 15:24:40 +0700

    feat(ui): new food centers location popup search

--- src/components/combobox-location.tsx
+++ src/components/combobox-location.tsx
@@ -17,34 +17,38 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import foodCenters from "@/data/food-center.json";
+import Image from "next/image";
+import Link from "next/link";
 
-const bars = [
-  {
-    value: "new bar",
-    label: "New bar",
-  },
-  {
-    value: "newer bar",
-    label: "Newer bar",
-  },
-  {
-    value: "old bar",
-    label: "Old bar",
-  },
-  {
-    value: "science bar",
-    label: "Science bar",
-  },
-  {
-    value: "engineering bar",
-    label: "Engineering bar",
-  },
-];
+// const bars = [
+//   {
+//     value: "new bar",
+//     label: "New bar",
+//   },
+//   {
+//     value: "newer bar",
+//     label: "Newer bar",
+//   },
+//   {
+//     value: "old bar",
+//     label: "Old bar",
+//   },
+//   {
+//     value: "science bar",
+//     label: "Science bar",
+//   },
+//   {
+//     value: "engineering bar",
+//     label: "Engineering bar",
+//   },
+// ];
 
 export function ComboboxDemo() {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const searchParams = useSearchParams();
+  const [value, setValue] = React.useState(searchParams.get("location") ?? "");
   const router = useRouter();
 
   return (
@@ -54,23 +58,53 @@ export function ComboboxDemo() {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-full justify-between bg-green-600 rounded-full text-white hover:bg-green-700 hover:text-white h-[45px]"
+          className="w-full flex justify-start bg-green-600 rounded-full text-white hover:bg-green-700 hover:text-white h-[45px]"
         >
           <MapPin className="w-4 h-4" />
-          {value
-            ? bars.find((bar) => bar.value === value)?.label
-            : "Where to eat?"}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          <span className="ml-2 whitespace-nowrap overflow-hidden w-[13ch] text-left">
+            {value
+              ? foodCenters.find(
+                  (bar) => bar.name.toLowerCase() === value.toLowerCase()
+                )?.name
+              : "Where to eat?"}
+          </span>
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[400px] p-0">
+        <div className="p-4">
+          <h2 className="font-semibold mb-3">Most Popular</h2>
+          <div className="flex gap-2 w-full">
+            {foodCenters
+              .filter((bar) => bar.popular)
+              .map((bar) => (
+                <button
+                  className="group block w-full relative overflow-hidden h-[175px] rounded-sm bg-black"
+                  key={bar.name}
+                  onClick={() => {
+                    router.push(`/search?location=${bar.name}`); // เปลี่ยนเส้นทางโดยเพิ่ม query parameter
+                  }}
+                >
+                  <Image
+                    fill={true}
+                    src={bar.image}
+                    alt={bar.name}
+                    className="w-full h-full object-cover opacity-75 group-hover:opacity-60 group-hover:scale-105 duration-300"
+                  />
+                  <span className="top-0 left-0 absolute text-sm font-semibold text-white p-2">
+                    {bar.shortName}
+                  </span>
+                </button>
+              ))}
+          </div>
+        </div>
         <Command>
           <CommandInput placeholder="Search bar..." />
           <CommandEmpty>No bar found.</CommandEmpty>
-          <CommandGroup>
-            {bars.map((bar) => (
+          <CommandGroup className="max-h-[400px] overflow-auto">
+            {foodCenters.map((bar) => (
               <CommandItem
-                key={bar.value}
+                key={bar.name}
                 onSelect={(currentValue) => {
                   setValue(currentValue === value ? "" : currentValue);
                   setOpen(false);
@@ -82,10 +116,10 @@ export function ComboboxDemo() {
                 <Check
                   className={cn(
                     "mr-2 h-4 w-4",
-                    value === bar.value ? "opacity-100" : "opacity-0"
+                    value === bar.name ? "opacity-100" : "opacity-0"
                   )}
                 />
-                {bar.label}
+                {bar.name}
               </CommandItem>
             ))}
           </CommandGroup>
