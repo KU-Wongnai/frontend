0af0394c8805bceabe4ae6b20be5e6282cdf1e98
0akkung <pawat.put@ku.th>
2023-11-07 09:12:41 +0700

    feat(restaurant): filter accepted restaurant

--- src/app/(user)/components/restaurant-list.tsx
+++ src/app/(user)/components/restaurant-list.tsx
@@ -44,9 +44,11 @@ const RestaurantList = () => {
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {restaurants.map((restaurant: Restaurant) => (
-            <RestaurantCard key={restaurant.id} {...restaurant} />
-          ))}
+          {
+              (restaurants as any)?.filter((r: any) => r.status === "ACCEPTED").map((restaurant: any) =>
+                        <RestaurantCard key={restaurant.id} {...restaurant} />
+              )
+          }
         </div>
       )}
     </>
